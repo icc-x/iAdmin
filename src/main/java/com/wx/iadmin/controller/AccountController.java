801090ac36a4bd621ab3885170b72592888246d5
icc <xinghun7@gmail.com>
2022-03-27 21:57:02 +0800

    init

--- src/main/java/com/wx/iadmin/controller/AccountController.java
+++ src/main/java/com/wx/iadmin/controller/AccountController.java
@@ -2,12 +2,14 @@ package com.wx.iadmin.controller;
 
 import com.wx.iadmin.dao.AccountRepository;
 import com.wx.iadmin.entity.Account;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.Predicate;
 import java.util.List;
 
 @RestController
@@ -17,14 +19,23 @@ public class AccountController {
     private AccountRepository accountRepository;
 
     @GetMapping("findAll")
-    public List<Account> findAll(){
+    public List<Account> findAll() {
         return accountRepository.findAll();
     }
 
+    @GetMapping("findByCode")
+    public Account findByCode(String code) {
+        Specification<Account> spec = (root, query, cb) -> {
+            Predicate p = cb.equal(root.get("code").as(String.class), code);
+            return query.where(p).getRestriction();
+        };
+        return accountRepository.findOne(spec).orElse(null);
+    }
+
     @RequestMapping("index")
-    public ModelAndView index(){
+    public ModelAndView index() {
         ModelAndView mv = new ModelAndView("index");
-        mv.addObject("name","hello 向胖子");
+        mv.addObject("name", "hello 向胖子");
         return mv;
     }
 }
